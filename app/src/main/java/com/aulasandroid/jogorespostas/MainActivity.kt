9ccdd21c770a07d3c36cbbfa0878ec4652e096b6
brenex920 <machado.breno127@gmail.com>
2026-03-23 16:58:03 -0300

    começo de projeto

---
+++ app/src/main/java/com/aulasandroid/jogorespostas/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.aulasandroid.jogorespostas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHost
+import androidx.navigation.NavType
+import androidx.navigation.compose.rememberNavController
+import com.aulasandroid.jogorespostas.screen.inicialScreen
+import com.aulasandroid.jogorespostas.ui.theme.JogoRespostasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JogoRespostasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    inicialScreen()
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/aulasandroid/jogorespostas/screen/InicialScreen.kt
@@ -0,0 +1,58 @@
+package com.aulasandroid.jogorespostas.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.aulasandroid.jogorespostas.R
+
+@Composable
+fun inicialScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .fillMaxHeight()
+            .background(Color.Blue)
+    ){
+        //header
+        Column(modifier = Modifier.fillMaxWidth()
+            .fillMaxHeight()
+            .background(color = colorResource(R.color.cor_app)),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Image(
+                painter = painterResource(R.drawable.quiz),
+                contentDescription = "imc logo",
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(vertical = 20.dp)
+
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.height(20.dp)
+                    .width(40.dp),
+            ) { }
+        }
+    }
+}
